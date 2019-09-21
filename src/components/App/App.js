891f0e2235f90024de2987b28a64e6535f892ce9
David Jimenez <david.jimenez@solera.com>
2019-09-21 22:50:17 +0200

    React and Redux - first approach - pass store to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <div className="app"></div>;
+import {
+  setBikes,
+  setBikesFilter,
+  addToCart,
+  removeFromCart,
+  checkoutCart,
+} from '../../store/actions';
+import { BIKE_FILTERS } from '../../constants';
+import { BikesService } from '../../services';
+
+const actions = [
+  setBikes(BikesService.getAllBikes()),
+  setBikesFilter(BIKE_FILTERS.MOUNTAIN),
+  addToCart(1),
+  addToCart(2),
+  removeFromCart(1),
+  setBikesFilter(BIKE_FILTERS.ROAD),
+  checkoutCart(),
+];
+
+class App extends Component {
+  state = { ...this.props.store.getState(), nextAction: 0 };
+
+  componentDidMount() {
+    const { store } = this.props;
+    this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  dispatchNextAction = () => {
+    const { store } = this.props;
+    const { nextAction } = this.state;
+    store.dispatch(actions[nextAction]);
+    this.setState({ nextAction: nextAction + 1 });
+  };
+
+  render() {
+    const { nextAction, ...state } = this.state;
+    return (
+      <div className="app">
+        <button
+          disabled={nextAction === actions.length}
+          onClick={this.dispatchNextAction}
+        >
+          Dispatch next action
+        </button>
+        <div>{`last action: ${
+          nextAction === 0 ? 'none' : JSON.stringify(actions[nextAction - 1])
+        }`}</div>
+        <div>{`state: ${JSON.stringify(state)}`}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,35 +5,6 @@ import App from './components/App';
 import './index.css';
 
 import { configureStore } from './store';
-import {
-  setBikes,
-  setBikesFilter,
-  addToCart,
-  removeFromCart,
-  checkoutCart,
-} from './store/actions';
-import { BIKE_FILTERS } from './constants';
-import { BikesService } from './services';
-
 const store = configureStore();
 
-const render = () => {
-  const { getState, dispatch } = store;
-  const props = {
-    ...getState(),
-    dispatch,
-  };
-  ReactDOM.render(<App {...props} />, document.getElementById('root'));
-};
-
-store.subscribe(render);
-
-render();
-
-store.dispatch(setBikes(BikesService.getAllBikes()));
-store.dispatch(setBikesFilter(BIKE_FILTERS.MOUNTAIN));
-store.dispatch(addToCart(1));
-store.dispatch(addToCart(2));
-store.dispatch(removeFromCart(1));
-store.dispatch(setBikesFilter(BIKE_FILTERS.ROAD));
-store.dispatch(checkoutCart());
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
