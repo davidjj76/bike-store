ce3ac53f8e176a2ad2c192beff2e53ce7e1cd29b
David Jimenez <david.jimenez@solera.com>
2019-09-21 21:31:35 +0200

    Create reducer II

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -7,48 +7,68 @@ const initialState = {
   cart: [],
 };
 
-export default function(state = initialState, action) {
+const updateItem = (items, updatedId, update) => {
+  return items.map(item => {
+    if (item.id === updatedId) {
+      return {
+        ...item,
+        ...update(item),
+      };
+    }
+    return item;
+  });
+};
+
+function bikes(state = initialState.bikes, action) {
   switch (action.type) {
     case TYPES.SET_BIKES:
-      return { ...state, bikes: action.bikes };
-
-    case TYPES.SET_FILTER:
-      return { ...state, filter: action.filter };
+      return action.bikes;
 
     case TYPES.ADD_TO_CART:
-      return {
-        ...state,
-        bikes: state.bikes.map(bike => {
-          if (bike.id === action.itemId) {
-            return {
-              ...bike,
-              stock: bike.stock - 1,
-            };
-          }
-          return bike;
-        }),
-        cart: [...state.cart, action.itemId],
-      };
+      return updateItem(state, action.itemId, item => ({
+        stock: item.stock - 1,
+      }));
 
     case TYPES.REMOVE_FROM_CART:
-      return {
-        ...state,
-        bikes: state.bikes.map(bike => {
-          if (bike.id === action.itemId) {
-            return {
-              ...bike,
-              stock: bike.stock + 1,
-            };
-          }
-          return bike;
-        }),
-        cart: state.cart.filter(item => item !== action.itemId),
-      };
-
-    case TYPES.CHECKOUT_CART:
-      return { ...state, cart: initialState.cart };
+      return updateItem(state, action.itemId, item => ({
+        stock: item.stock + 1,
+      }));
 
     default:
       return state;
   }
+}
+
+function filter(state = initialState.filter, action) {
+  switch (action.type) {
+    case TYPES.SET_FILTER:
+      return action.filter;
+
+    default:
+      return state;
+  }
+}
+
+function cart(state = initialState.cart, action) {
+  switch (action.type) {
+    case TYPES.ADD_TO_CART:
+      return [...state, action.itemId];
+
+    case TYPES.REMOVE_FROM_CART:
+      return state.filter(item => item !== action.itemId);
+
+    case TYPES.CHECKOUT_CART:
+      return initialState.cart;
+
+    default:
+      return state;
+  }
+}
+
+export default function(state = initialState, action) {
+  return {
+    bikes: bikes(state.bikes, action),
+    filter: filter(state.filter, action),
+    cart: cart(state.cart, action),
+  };
 }
