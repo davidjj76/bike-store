e4085de09f250c939fa06eef3f602625259427fb
David Jiménez <davidjj76@gmail.com>
2019-11-26 23:43:25 +0100

    Accion removeFromCart

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 
 import Cart from './Cart';
 import { getCartItems } from '../../store/selectors';
+import { removeFromCart } from '../../store/actions';
 
 function mapStateToProps(state) {
   return {
@@ -9,4 +10,14 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(Cart);
+function mapDispatchToProps(dispatch) {
+  return {
+    removeFromCart: (itemId, quantity) =>
+      dispatch(removeFromCart(itemId, quantity)),
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Cart);
