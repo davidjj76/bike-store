b4046b38aab6eb679d27907ec9e4ca048732e255
David Jiménez <davidjj76@gmail.com>
2020-05-12 22:24:07 +0200

    First selectors!

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,19 +2,24 @@ import { connect } from 'react-redux';
 
 import App from './App';
 import { setBikes } from '../../store/actions';
+import { getBikes, getFilter } from '../../store/selectors';
 
 function mapStateToProps(state, ownProps) {
   return {
-    bikes: state.bikes,
-    bikesFilter: state.filter,
+    bikes: getBikes(state),
+    bikesFilter: getFilter(state),
   };
 }
 
-function mapDispatchToProps(dispatch, ownProps) {
-  return {
-    loadBikes: bikes => dispatch(setBikes(bikes)),
-  };
-}
+// function mapDispatchToProps(dispatch, ownProps) {
+//   return {
+//     loadBikes: bikes => dispatch(setBikes(bikes)),
+//   };
+// }
+
+const mapDispatchToProps = {
+  loadBikes: setBikes,
+};
 
 const connected = connect(mapStateToProps, mapDispatchToProps);
 const AppConnected = connected(App);
