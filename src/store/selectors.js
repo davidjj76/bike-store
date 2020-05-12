84a7ece25b031b6ce95a928ba10607970f768dea
David Jiménez <davidjj76@gmail.com>
2020-05-12 23:44:39 +0200

    Calculate visible bikes

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,3 +1,15 @@
+import { BIKE_FILTERS } from '../constants';
+
 export const getBikes = state => state.bikes;
 export const getFilter = state => state.filter;
 export const isActiveFilter = (state, filter) => state.filter === filter;
+
+export function getVisibleBikes(state) {
+  const bikes = getBikes(state);
+  const filter = getFilter(state);
+  let visibleBikes = bikes;
+  if (filter !== BIKE_FILTERS.ALL) {
+    visibleBikes = bikes.filter(bike => bike.type === filter);
+  }
+  return visibleBikes.map(bike => ({ ...bike, hasStock: bike.stock > 0 }));
+}
