de5d6baa903e6a2b45785e0daa23fc7c3446849f
David Jiménez <davidjj76@gmail.com>
2020-05-12 21:57:13 +0200

    Connected App!

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,1 +1,22 @@
-export { default } from './App';
+import { connect } from 'react-redux';
+
+import App from './App';
+import { setBikes } from '../../store/actions';
+
+function mapStateToProps(state, ownProps) {
+  return {
+    bikes: state.bikes,
+    bikesFilter: state.filter,
+  };
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    loadBikes: bikes => dispatch(setBikes(bikes)),
+  };
+}
+
+const connected = connect(mapStateToProps, mapDispatchToProps);
+const AppConnected = connected(App);
+
+export default AppConnected;
