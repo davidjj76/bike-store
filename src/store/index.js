87261dfa88c95a4eba2300273b0251208acc53a5
David Jiménez <davidjj76@gmail.com>
2020-05-14 00:41:54 +0200

    Connect Router and Redux with Connected React Router

--- src/store/index.js
+++ src/store/index.js
@@ -2,26 +2,29 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
 import ReduxLogger from 'redux-logger';
+import { connectRouter } from 'connected-react-router';
 
 import * as reducers from './reducers';
 
-const reducer = combineReducers(reducers);
+const createRootReducer = ({ history }) =>
+  combineReducers({
+    router: connectRouter(history),
+    ...reducers,
+  });
+
+const configureMiddleware = config => [
+  ReduxThunk.withExtraArgument(config),
+  ReduxLogger,
+];
 
 const composeEnhancers = composeWithDevTools;
 
-const configureMiddleware = config => {
-  const middlewares = [ReduxThunk.withExtraArgument(config), ReduxLogger];
-  return middlewares;
-};
-
 export function configureStore(config) {
   return function (preloadedState) {
+    const reducer = createRootReducer(config);
     const middlewares = configureMiddleware(config);
-    const store = createStore(
-      reducer,
-      preloadedState,
-      composeEnhancers(applyMiddleware(...middlewares))
-    );
+    const enhancers = composeEnhancers(applyMiddleware(...middlewares));
+    const store = createStore(reducer, preloadedState, enhancers);
     return store;
   };
 }
